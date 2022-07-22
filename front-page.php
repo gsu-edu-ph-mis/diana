08e918add6ab7ebbedc57391dcaaeffe6c57fe9f
kosinix <kosinix@users.noreply.github.com>
2022-07-22 22:30:31 +0800

    Change to 28 courses

--- front-page.php
+++ front-page.php
@@ -131,7 +131,7 @@ get_header(); ?>
 						<path fill="currentColor" d="M18,22A2,2 0 0,0 20,20V4C20,2.89 19.1,2 18,2H12V9L9.5,7.5L7,9V2H6A2,2 0 0,0 4,4V20A2,2 0 0,0 6,22H18Z" />
 					</svg>
 				</div>
-				<h2 class="counters" style="font-size: 0; min-height: 33.6px;">26</h2>
+				<h2 class="counters" style="font-size: 0; min-height: 33.6px;">28</h2>
 				<h3 class="h2">Programs Offered</h3>
 			</div>
 		</div>
