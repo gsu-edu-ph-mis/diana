029bba84029cb3066cfb32bba3f97cfdd938f101
kosinix <kosinix@users.noreply.github.com>
2022-08-23 08:29:42 +0800

    Add between date

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -379,7 +379,15 @@ function kalendaryo_get_suffix($momentDate, $weekStart = 0) {
     }, $suffix);
     return $suffix;
 }
-
+/**
+ * @param DateTime $date Date that is to be checked if it falls between $startDate and $endDate
+ * @param DateTime $startDate Date should be after this date to return true
+ * @param DateTime $endDate Date should be before this date to return true
+ * return bool
+ */
+function isDateBetweenDates(DateTime $date, DateTime $startDate, DateTime $endDate) {
+    return $date > $startDate && $date < $endDate;
+}
 
 function kalendaryo_get_days($momentDate, $weekStart = 0) {
 
