b8ee7b8b93513fa5e1b2bb6a73b85d4da0395ae0
kosinix <kosinix@users.noreply.github.com>
2023-01-30 08:21:08 +0800

    Add far 4 december 2022

--- page-transparency-seal.php
+++ page-transparency-seal.php
@@ -107,7 +107,8 @@ get_header(); ?>
 										<a href="https://drive.google.com/file/d/1_7QBZN1WCLtsn1V8mg9Octzi9oZOkq1B/preview">Aug</a>,
 										<a href="https://drive.google.com/file/d/1YGIwqBzmoLrWY8ZxKvYCQnzvU78hrAtO/preview">Sep</a>,
 										<a href="https://drive.google.com/file/d/1UTAqptakxCPc7TmsEtzmWGwMV7tb9_Rk/preview">Oct</a>,
-										<a href="https://drive.google.com/file/d/1DDYqvZ2G-CLkbVjev0VIwkHU4JPg2EAR/preview">Nov</a>
+										<a href="https://drive.google.com/file/d/1DDYqvZ2G-CLkbVjev0VIwkHU4JPg2EAR/preview">Nov</a>,
+										<a href="https://drive.google.com/file/d/1u9KQlxCNtVF-0wjhMwZ8G27ZNRr8_LaZ/preview">Dec</a>
 									</li>
 									<li>FY 2021 - 
 										<a href="https://drive.google.com/file/d/1O7vK_-E4knXkizU0pxayWAfhmYQzGSFU/preview">Jan</a>, 
