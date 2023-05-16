b0e1746d2d2a799c1090e8bb93abbc1da5e06998
kosinix <kosinix@users.noreply.github.com>
2023-05-16 10:01:32 +0800

    Add width height

--- page-board-of-regents.php
+++ page-board-of-regents.php
@@ -28,7 +28,7 @@ get_header(); ?>
 						
 						<div class="col-md-6 official">
 							<div class="official-photo mb-3">
-								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/prospero.devera.jpg" alt="Profile Photo">
+								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/prospero.devera.jpg" width="250" height="250" alt="Profile Photo">
 							</div>
 							<h3 class="h3">Hon. J. Prospero E. De Vera, III</h3>
 							<h4 class="h4">Chairman, Commission on Higher Education</h4>
@@ -36,7 +36,7 @@ get_header(); ?>
 						</div>
 						<div class="col-md-6 official">
 							<div class="official-photo mb-3">
-								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/lilian-parreno.jpg" alt="Lilian">
+								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/lilian-parreno.jpg" width="250" height="250" alt="Lilian">
 							</div>
 							<h3 class="h3">Hon. Lilian Diana B. Parreño</h3>
 							<h4 class="h4">University President</h4>
@@ -46,7 +46,7 @@ get_header(); ?>
 					<div class="row mb-5">
 						<div class="col-md-6 official ">
 							<div class="official-photo-2 mb-3">
-								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/xxx.jpg" alt="Profile Photo">
+								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/xxx.jpg" width="250" height="250" alt="Profile Photo">
 							</div>
 							<h3 class="h3">Hon. Francis "Chiz" G. Escudero</h3>
 							<h4 class="h4">Chairman, Senate Committee on Higher and Technical Education</h4>
@@ -55,7 +55,7 @@ get_header(); ?>
 						</div>
 						<div class="col-md-6 official ">
 							<div class="official-photo-2 mb-3">
-								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/mark.go.jpg" alt="Profile Photo">
+								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/mark.go.jpg" width="250" height="250" alt="Profile Photo">
 							</div>
 							<h3 class="h3">Hon. Mark O. Go</h3>
 							<h4 class="h4">Chairman, Committee on Higher and Technical Education of the House of Representatives</h4>
@@ -66,7 +66,7 @@ get_header(); ?>
 					<div class="row mb-5">
 						<div class="col-md-4 official ">
 							<div class="official-photo-2 mb-3">
-								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/arecio.casing.jpg" alt="Profile Photo">
+								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/arecio.casing.jpg" width="250" height="250" alt="Profile Photo">
 							</div>
 							<h3 class="h3">Hon. Arecio A. Casing, Jr.</h3>
 							<h4 class="h4">Regional Director, National Economic & Development Authority (NEDA) Region VI – Western Visayas</h4>
@@ -74,7 +74,7 @@ get_header(); ?>
 						</div>
 						<div class="col-md-4 official ">
 							<div class="official-photo-2 mb-3">
-								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/rowen.gelonga.jpg" alt="Profile Photo">
+								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/rowen.gelonga.jpg" width="250" height="250" alt="Profile Photo">
 							</div>
 							<h3 class="h3">Hon. Rowen R. Gelonga</h3>
 							<h4 class="h4">Regional Director, Department of Science and Technology (DOST) Region VI – Western Visayas
@@ -82,7 +82,7 @@ get_header(); ?>
 						</div>
 						<div class="col-md-4 official ">
 							<div class="official-photo-2 mb-3">
-								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/barrogo.albert.jpg" alt="Profile Photo">
+								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/barrogo.albert.jpg" width="250" height="250" alt="Profile Photo">
 							</div>
 							<h3 class="h3">Hon. Jose Albert A. Barrogo</h3>
 							<h4 class="h4">Regional Director, Department of Agriculture (DA) Region VI – Western Visayas</h4>
@@ -90,7 +90,7 @@ get_header(); ?>
 						</div>
 						<div class="col-md-4 official ">
 							<div class="official-photo-2 mb-3">
-								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/xxx.jpg" alt="Profile Photo">
+								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/xxx.jpg" width="250" height="250" alt="Profile Photo">
 							</div>
 							<h3 class="h3">Hon. Dionne B. Fernandez</h3>
 							<h4 class="h4">Private Sector Representative</h4>
@@ -98,7 +98,7 @@ get_header(); ?>
 						</div>
 						<div class="col-md-4 official ">
 							<div class="official-photo-2 mb-3">
-								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/xxx.jpg" alt="Profile Photo">
+								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/xxx.jpg" width="250" height="250" alt="Profile Photo">
 							</div>
 							<h3 class="h3">Hon. Sanie D. Ong</h3>
 							<h4 class="h4">Private Sector Representative</h4>
@@ -107,7 +107,7 @@ get_header(); ?>
 						
 						<div class="col-md-4 official ">
 							<div class="official-photo-2 mb-3">
-								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/erly.martir.jpg" alt="Profile Photo">
+								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/erly.martir.jpg" width="250" height="250" alt="Profile Photo">
 							</div>
 							<h3 class="h3">Hon. Erly M. Martir</h3>
 							<h4 class="h4">Representative, Federated Alumni Association</h4>
@@ -115,7 +115,7 @@ get_header(); ?>
 						</div>
 						<div class="col-md-4 official ">
 							<div class="official-photo-2 mb-3">
-								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/jonathan.gabion.jpg" alt="Profile Photo">
+								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/jonathan.gabion.jpg" width="250" height="250" alt="Profile Photo">
 							</div>
 							<h3 class="h3">Hon. Jonathan G. Gabion</h3>
 							<h4 class="h4">President, Federated Faculty Association</h4>
@@ -123,7 +123,7 @@ get_header(); ?>
 						</div>
 						<div class="col-md-4 official ">
 							<div class="official-photo-2 mb-3">
-								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/celine.eulin.jpg" alt="Profile Photo">
+								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/celine.eulin.jpg" width="250" height="250" alt="Profile Photo">
 							</div>
 							<h3 class="h3">Hon. Celine D. Eulin</h3>
 							<h4 class="h4">President, Federated Student Republic</h4>
@@ -139,7 +139,7 @@ get_header(); ?>
 						</div> -->
 						<div class="col-md-4 official ">
 							<div class="official-photo-2 mb-3">
-								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/janet.aleman.jpg" alt="Profile Photo">
+								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/janet.aleman.jpg" width="250" height="250" alt="Profile Photo">
 							</div>
 							<h3 class="h3">Mrs. Janet R. Aleman</h3>
 							<h4 class="h4">University Board Secretary</h4>
