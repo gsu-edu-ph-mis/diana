be9b73a1252ea40ff0b2c5b2eb7c6c149ee84b87
kosinix <kosinix@users.noreply.github.com>
2023-06-16 22:53:28 +0800

    Remove new tab

--- page-frames.php
+++ page-frames.php
@@ -34,7 +34,7 @@ get_header(); ?>
 						<img v-if="photo" v-bind:src="photo" alt="Frame">
 						<div class="d-flex">
 							<button type="button" v-if="photo" class="btn-full-width mt-3 mb-5 mr-1 btn btn-lg btn-warning" v-on:click="download">Download</button>
-							<button type="button" v-if="photo" class="btn-full-width mt-3 mb-5 ml-1 btn btn-lg btn-secondary" v-on:click="openTab(photo)">Open in new Tab</button>
+							<!-- <button type="button" v-if="photo" class="btn-full-width mt-3 mb-5 ml-1 btn btn-lg btn-secondary" v-on:click="openTab(photo)">Open in new Tab</button> -->
 						</div>
 
 					</div>
