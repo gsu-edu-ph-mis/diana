7bae02fe457b1f314abe691a35313faffdb3f27a
kosinix <kosinix@users.noreply.github.com>
2023-08-25 11:22:27 +0800

    Update brief history

--- page-history.php
+++ page-history.php
@@ -38,15 +38,15 @@ get_header(); ?>
 					<div class="important event">
 						<h2 class="h2">1968</h2>
 						<p><img src="<?= get_stylesheet_directory_uri(); ?>/images/history/marker1968.jpg" alt=""></p>
-						<p>Four years after, through the efforts of Atty. Ernesto Gaduyon and Governor Abelardo Javellana, the philanthropist, Atty. Juan Salvador, donated 50,000 square meter lot of his estate in Barangay Mclain, Buenavista, Guimaras, to be the school site. Buenavista Vocational School was officially opened.</p>
+						<p>Four years later, through the efforts of Atty. Ernesto Gaduyon and Governor Abelardo Javellana, the philanthropist Atty. Juan Salvador donated a 50,000-square-meter lot from his estate in Barangay Mclain, Buenavista, Guimaras, to serve as the school site. As a result, Buenavista Vocational School was officially opened.</p>
 					</div>
 					<div class="event right">
 						<h2 class="h2">1980</h2>
-						<p>In 1980, the school was granted a permit to offer post-secondary courses. The two-year trade technical and technology courses paved the way for the offering of higher education courses, which are either terminal or ladderized courses. </p>
+						<p>In 1980, the school was granted a permit to offer post-secondary courses. The two-year trade technical and technology courses paved the way for the offering of higher education courses, which include both terminal and ladderized programs.</p>
 					</div>
 					<div class="event">
 						<h2 class="h2">1992</h2>
-						<p> In 1992, the Sangguniang Bayan of Buenavista passed a resolution requesting Representative Alberto Lopez of the second district of Iloilo to work for the conversion of BVS to Guimaras Polytechnic College.</p>
+						<p>In 1992, the Sangguniang Bayan of Buenavista passed a resolution requesting Representative Alberto Lopez from the second district of Iloilo to work towards the conversion of Buenavista Vocational School into Guimaras Polytechnic College.</p>
 					</div>
 					<div class="event right">
 						<h2 class="h2">1994</h2>
@@ -58,18 +58,32 @@ get_header(); ?>
 					</div>
 					<div class="event right">
 						<h2 class="h2">2000</h2>
-						<p>In June 2000, a consultation and public hearing on House Bill nos. 7382 and 5807 sponsored by Representative Emily R. Lopez of the Lone District of Guimaras was conducted at the GPC Function Room. </p>
-						<p>With 179 registered participants from public and private sectors in attendance, all sectoral representatives expressed full support for the conversion of GPC, Buenavista and Western Visayas College of Science and Technology (WVCST)-Mosqueda Campus in the Municipality of Jordan, into a State College.</p>
+						<p>In June 2000, Representative Emily R. Lopez of the Lone District of Guimaras held a consultation and public hearing on House Bills 7382 and 5807. 179 public and private sector representatives supported the conversion of GPC, Buenavista, and Western Visayas College of Science and Technology (WVCST)-Mosqueda Campus in Jordan into a State College.</p>
 					</div>
 					<div class="important event">
 						<h2 class="h2">2001</h2>
-						<p>Then, in the eleventh congress first regular session, Congressman Dante V. Liban and Congresswoman Emily R. Lopez filed House Bill 12358 (in substitution of House Bills 5807 & 7382). </p>
-						<p>On June 8, 2001, the long cherished dream of GPC community became a reality with the signing into law by President Gloria M. Arroyo of Republic Act 9138, establishing the Guimaras State College, integrating therewith the Guimaras Polytechnic College in the Municipality of Buenavista, and the Western Visayas College of Science and Technology – Guimaras Extension in the Municipality of Jordan. </p>
-
-						<p>GSC is mandated to offer undergraduate and graduate courses in technology education, agriculture, fishery, engineering, arts and sciences, forestry, business, health, computer, criminology, nautical and short-term vocational-technical and other continuing courses that may be found to be needed and relevant.</p>
+						<p>Dante V. Liban and Emily R. Lopez filed House Bill 12358 in the eleventh Congress' first regular session (in substitution of House Bills 5807 & 7382). </p>
+						<p>On June 8, 2001, President Gloria M. Arroyo signed Republic Act 9138, creating the Guimaras State College by merging the Guimaras Polytechnic College in Buenavista and the Western Visayas College of Science and Technology - Guimaras Extension in Jordan.</p>
+					</div>
+					<div class="event right">
+						<h2 class="h2">2012</h2>
+						<p><a target="_blank" href="<?= get_stylesheet_directory_uri(); ?>/files/HB06634.pdf"><img src="<?= get_stylesheet_directory_uri(); ?>/images/history/HB06634.jpg" alt="HB06634"></a><br>The 15th Congress enacted House Bill 06634 (An Act Converting the Guimaras State College in The Province of Guimaras Into a State University to Be Known as The Guimaras State University and Appropriating Funds Therefor) by former Congressman Joaquin Carlos Rahman Nava.</p>
+					</div>
+					<div class="event ">
+						<h2 class="h2">2014</h2>
+						<p>Cong. Rahman Nava refiled HB 05181 in the 16th Congress, which the House approved and sent to the Senate in December 2014.</p>
+						<p>17th Congress - Hon. Ma. Lucille Ledesma Nava sponsored HB 07953 to convert GSC to GSU.</p>
+					</div>
+					<div class="event right">
+						<h2 class="h2">2019</h2>
+						<p><a target="_blank" href="<?= get_stylesheet_directory_uri(); ?>/files/20190426-RA-11335-RRD.pdf"><img src="<?= get_stylesheet_directory_uri(); ?>/images/history/20190426-RA-11335-RRD.jpg" alt="20190426-RA-11335-RRD"></a> <br> President Rodrigo R. Duterte signed Republic Act 11335 on April 26, 2019.</p>
+					</div>
+					<div class="important event">
+						<h2 class="h2">2022</h2>
+						<p>In June 7, 2022, the CHED Commission en Banc confirmed the conversion of Guimaras State College into Guimaras State University.</p>
 					</div>
 				</div>
-				<p class="h1 text-center text-md-left p-0 p-md-5">At last, Guimarasnons have the opportunity to be educated and become professionals right in their own turf, spending less for quality education.</p>
+				<p class="h1 text-center text-md-left p-0 p-md-5">At last, the people of Guimaras have the chance to get educated and become professionals right in their own locale, spending less for quality education.</p>
 			</div>
 			<div class="col-md-3 order-2 order-md-1 mb-4">
 				<nav class="page-sub-menu text-center scrolling-nav mb-5">
