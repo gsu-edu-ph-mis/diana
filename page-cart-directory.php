0060f8b1c08a000b88f27d5b55e6d2a8922ccb65
kosinix <kosinix@users.noreply.github.com>
2024-01-18 10:23:20 +0800

    Add cart

---
+++ page-cart-directory.php
@@ -0,0 +1,170 @@
+<?php
+/**
+ * Template Name: CART Directory
+ *
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Diana
+ * subpackage Diana 1.0
+ */
+
+get_header(); ?>
+<div class="section section-page-title">
+	<div class="container">
+		<h1 class="page-title"><?php single_post_title(); ?></h1>
+	</div>
+</div>
+<div class="site-content">
+	<div class="container">
+		<main id="main" class="row pt-5 pb-5  text-left" role="main">
+			<div class="col-md-12 order-2">
+				
+				<table class="table table-sm  table-striped mb-5 border" style="max-width: 500px">
+					<tr>
+						<td class="font-weight-bold">Name of Agency</td>
+						<td>Guimaras State University	</td>
+					</tr>
+					<tr>
+						<td class="font-weight-bold">Department Order</td>
+						<td>Memorandum Order No. 05, series of 2024	</td>
+					</tr>
+					<tr>
+						<td class="font-weight-bold">CART or SUBCART</td>
+						<td>CART</td>
+					</tr>
+				</table>
+				<div class="table-responsive">
+					
+					<table class="table table-sm table-striped">
+						<tr>
+							<th>NO.</th>
+							<th>NAME</th>
+							<th>CART DESIGNATION</th>
+							<th class="d-none d-md-table-cell">POSITION / DESIGNATION / TITLE</th>
+							<th class="d-none d-md-table-cell">OFFICE/ DEPARTMENT/ DIVISION</th>
+							<th>EMAIL ADDRESS</th>
+							<th>CONTACT NUMBER</th>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>Dr. Luna V. Lamera</td>
+							<td>Chairperson</td>
+							<td class="d-none d-md-table-cell">Chief Administrative Officer - Administrative Division</td>
+							<td class="d-none d-md-table-cell">Office of the Chief Administrative Officer - Administrative Division</td>
+							<td>luna.lamera@gsu.edu.ph</td>
+							<td>09516926710</td>
+						</tr>
+						<tr>
+							<td>2</td>
+							<td>Dr. Efren S. Tellermo</td>
+							<td>Vice Chairperson</td>
+							<td class="d-none d-md-table-cell">Planning Director</td>
+							<td class="d-none d-md-table-cell">Planning Office</td>
+							<td>efren.tellermo@gsu.edu.ph</td>
+							<td>09778330255</td>
+						</tr>
+						<tr>
+							<td>3</td>
+							<td>Instr. Violeta C. Efondo</td>
+							<td>Member</td>
+							<td class="d-none d-md-table-cell">Executive Assistant on Legal Matters</td>
+							<td class="d-none d-md-table-cell">Office of the Executive Assistant on Legal Matters</td>
+							<td>violeta.efondo@gsu.edu.ph</td>
+							<td>09196985082</td>
+						</tr>
+						<tr>
+							<td>4</td>
+							<td>Dr. Sol M. Basañes</td>
+							<td>Member</td>
+							<td class="d-none d-md-table-cell">Records Officer</td>
+							<td class="d-none d-md-table-cell">Records Office</td>
+							<td>sol.basanes@gsu.edu.ph</td>
+							<td>09756759548</td>
+						</tr>
+						<tr>
+							<td>5</td>
+							<td>Dr. Ann Lyn S. Tumapang</td>
+							<td>Member</td>
+							<td class="d-none d-md-table-cell">Chief Administrative Officer, Finance Division</td>
+							<td class="d-none d-md-table-cell">Office of the Chief Administrative Officer, Finance Division</td>
+							<td>cao.finance@gsu.edu.ph</td>
+							<td>09173228113</td>
+						</tr>
+						<tr>
+							<td>6</td>
+							<td>Mr. Philippe R. Prologo</td>
+							<td>Member</td>
+							<td class="d-none d-md-table-cell">Supervising Administrative Officer, Finance Division</td>
+							<td class="d-none d-md-table-cell">Office of the Supervising Administrative Officer, Finance Division</td>
+							<td>accounting@gsu.edu.ph</td>
+							<td>09951929410</td>
+						</tr>
+						<tr>
+							<td>7</td>
+							<td>Mrs. Ma. Rechel A. Pillora</td>
+							<td>Member</td>
+							<td class="d-none d-md-table-cell">Administrative Officer V (Human Resource and Management Officer)</td>
+							<td class="d-none d-md-table-cell">Human Resource and Management Office</td>
+							<td>marechel.pillora@gsu.edu.ph</td>
+							<td>09171564778</td>
+						</tr>
+						<tr>
+							<td>8</td>
+							<td>Mr. Nestor T. Paez</td>
+							<td>Member</td>
+							<td class="d-none d-md-table-cell">Casual Plantilla</td>
+							<td class="d-none d-md-table-cell">Public Assistance Complaints Desk</td>
+							<td>nestor.paez@gsu.edu.ph</td>
+							<td>09978279411</td>
+						</tr>
+						<tr>
+							<td>9</td>
+							<td>Dr. Adrian J. Forca</td>
+							<td>Member</td>
+							<td class="d-none d-md-table-cell">Director, Information, Communication and Technology Officer</td>
+							<td class="d-none d-md-table-cell">Office of the Director, Information, Communication and Technology</td>
+							<td>adrian.forca@gsu.edu.ph</td>
+							<td>09177106115</td>
+						</tr>
+						<tr>
+							<td>10</td>
+							<td>Mrs. Grace D. Ello</td>
+							<td>Secretariat</td>
+							<td class="d-none d-md-table-cell">Administrative Aide VI</td>
+							<td class="d-none d-md-table-cell">Human Resource and Management Office</td>
+							<td>grace.ello@gsu.edu.ph</td>
+							<td>09359604220</td>
+						</tr>
+						<tr>
+							<td>11</td>
+							<td>Dr. Julieta G. Infante</td>
+							<td>Focal Person - Baterna Campus</td>
+							<td class="d-none d-md-table-cell">Director, Baterna Campus</td>
+							<td class="d-none d-md-table-cell">Office of the Director, Baterna Campus</td>
+							<td>julieta.infante@gsu.edu.ph</td>
+							<td>09194939399</td>
+						</tr>
+						<tr>
+							<td>12</td>
+							<td>Dr. Kert D. Pillora</td>
+							<td>Focal Person - Mosqueda Campus</td>
+							<td class="d-none d-md-table-cell">Director, Mosqueda Campus</td>
+							<td class="d-none d-md-table-cell">Office of the Director, Mosqueda Campus</td>
+							<td>kert.pillora@gsu.edu.ph/ mosqueda@gsu.edu.ph</td>
+							<td>09171500805</td>
+						</tr>
+					</table>
+				</div>
+				<p class="mb-5 text-right"><small><em>References: 
+					<a href="https://drive.google.com/file/d/1cqnWFvgM1J0-gaZgHenwHYtEf9nIo856/preview">Guimaras State University Mail - Posting in Websites for CART compliance.pdf</a></em></small>
+				</p>
+			</div>
+		</main>
+	</div><!-- .container -->
+</div>
+<?php get_footer(); ?>
