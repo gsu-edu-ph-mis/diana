678175921724571da03125f682d37c9c50e20fc7
kosinix <kosinix@users.noreply.github.com>
2024-01-26 16:58:41 +0800

    Remove CIMD director message

--- page-cimd.php
+++ page-cimd.php
@@ -33,14 +33,14 @@ get_header(); ?>
 			<div class="col-lg-6">
 				<h2 class="h2 mb-3">About the Center</h2>
 				<p>The Center for Instructional Materials Development (CIMD) assists faculty authors in meticulously planning, selecting, organizing, refining, and utilizing instructional material (IM) to maximize its effectiveness. It ensures the breadth and depth of content to maximize student learning. CIMD directs the editorial direction of instructional materials and their content, as well as digital content, in order to promote equity of access, effectiveness of teaching and learning, and curriculum enhancement, with the dual goal of increasing student success and ensuring that graduates of the Guimaras State College are globally competitive, locally relevant, socially responsive, and fully representative of the country's diverse population. CIMD's content is managed by teams of editors, designers, industry experts, and freelance contributors.</p>
-				<h2 class="h2 mb-3">Message from the Director</h2>
+				<!-- <h2 class="h2 mb-3">Message from the Director</h2>
                 <p>"In this day and age, writing IMs (Instructional Materials) has something to do with the fact that most people are juggling a variety of responsibilities such as family, career, and personal lives.</p>
                 <p>I've discovered that everything is true. </p>
                 <p>Since they can't see them in person, instructors and professors have to convey all of their meaning through their textbooks and other forms of electronic communication; they can't gaze into their students' eyes, which are the windows into their hearts. 
                 </p>
-                <p>Because they aren't familiar with the appearance of these students, IMs are, in my opinion, important vehicles for enabling education to produce fully formed and well-prepared individuals while also serving as a lever for social change."</p>
+                <p>Because they aren't familiar with the appearance of these students, IMs are, in my opinion, important vehicles for enabling education to produce fully formed and well-prepared individuals while also serving as a lever for social change."</p> -->
 			</div>
-			<div class="col-lg-3 text-center">
+			<!-- <div class="col-lg-3 text-center">
 				<div class="official">
 					<div class="official-photo-2">
 						<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/roger.rueda.jpg" alt="Roger">
@@ -50,8 +50,8 @@ get_header(); ?>
 					~ <strong>Dr. Roger B. Rueda</strong> <br>
 					<em>Director, Center for Instructional Materials Development</em>
 				</p>
-			</div>
-			<div class="col-12">
+			</div> -->
+			<!-- <div class="col-12">
 				<div class="row">
 					<div class="col-sm-3 mt-3 mb-3">
 						<div class="card">
@@ -82,7 +82,7 @@ get_header(); ?>
 						</div>
 					</div>
 				</div>
-			</div>
+			</div> -->
 		</main>
 	</div><!-- .container -->
 </div>
