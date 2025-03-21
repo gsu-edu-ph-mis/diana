545d3e8518440fbd0747852992ed01f04a511ad2
kosinix <kosinix@users.noreply.github.com>
2025-03-21 13:42:43 +0800

    Add

--- page-key-officials.php
+++ page-key-officials.php
@@ -695,14 +695,14 @@ get_header(); ?>
 						
 						<div class="col-md-4 official ">
 							<div class="official-photo-2 mb-3">
-								<img @click="toggle($event)" data-no="" src="<?= get_stylesheet_directory_uri(); ?>/images/officials/zzz.jpg" data-me="<?= get_stylesheet_directory_uri(); ?>/images/officials/bessie.cebuano.jpg" width="250" height="250" alt="Profile">
+								<img @click="toggle($event)" data-no="" src="<?= get_stylesheet_directory_uri(); ?>/images/officials/zzz.jpg" data-me="<?= get_stylesheet_directory_uri(); ?>/images/officials/vail.nagaynay.jpg" width="250" height="250" alt="Profile">
 							</div>
-							<h3 class="h3">Bessie S. Cebuano</h3>
-							<h4 class="h4">Head, Legal Unit / Legal Assistant III</h4>
+							<h3 class="h3">Atty. Vail Michael P. Nagaynay</h3>
+							<h4 class="h4">Head, Legal Unit / Attorney IV</h4>
 							<div class="h5">	
 								<a href="#">
 									<svg style="width:16px;height:16px" viewBox="0 0 24 24"><path fill="currentColor" d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z" /></svg>
-									<span @click.prevent=toggleM($event) data-show="orffvr.prohnab@tfh.rqh.cu">***click to show email***</span>
+									<span @click.prevent=toggleM($event) data-show="invyzvpunry.antnlanl@tfh.rqh.cu">***click to show email***</span>
 								</a>		
 							</div>
 						</div>
