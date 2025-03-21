98997e40c8bd67f5a4ca8e7281c0aac8718585a3
kosinix <kosinix@users.noreply.github.com>
2025-03-21 15:40:35 +0800

    Updated board

--- page-board-of-regents.php
+++ page-board-of-regents.php
@@ -31,8 +31,8 @@ get_header(); ?>
 								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/prospero.devera.jpg" width="250" height="250" alt="Profile Photo">
 							</div>
 							<h3 class="h3">Hon. J. Prospero E. De Vera, III</h3>
-							<h4 class="h4">Chairman, Commission on Higher Education</h4>
-							<h4 class="h5">Chairman, GSU Board of Regents</h4>
+							<h4 class="h4">Chairperson, Commission on Higher Education</h4>
+							<h4 class="h5">Chairperson, GSU Board of Regents</h4>
 						</div>
 						<div class="col-md-6 official">
 							<div class="official-photo mb-3">
@@ -40,25 +40,25 @@ get_header(); ?>
 							</div>
 							<h3 class="h3">Hon. Lilian Diana B. Parreño</h3>
 							<h4 class="h4">University President</h4>
-							<h4 class="h5">Vice Chair, GSU Board of Regents</h4>
+							<h4 class="h5">Vice Chairperson, GSU Board of Regents</h4>
 						</div>
 					</div>
 					<div class="row mb-5">
 						<div class="col-md-6 official ">
 							<div class="official-photo-2 mb-3">
-								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/chiz.escudero.jpg" width="250" height="250" alt="Profile Photo">
+								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/allan.cayetano.jpg" width="250" height="250" alt="Profile Photo">
 							</div>
-							<h3 class="h3">Hon. Francis "Chiz" G. Escudero</h3>
-							<h4 class="h4">Chairman, Senate Committee on Higher and Technical Education</h4>
+							<h3 class="h3">Hon. Allan Peter Cayetano</h3>
+							<h4 class="h4">Chairperson, Senate Committee on Higher, Technical, and Vocational Education</h4>
 							<h4 class="h5">Member</h4>
-							<h4 class="h4">Represented by: <strong>Hon. Rani E. Rodriguez</strong></h4>
+							<!-- <h4 class="h4">Represented by: <strong>Hon. Rani E. Rodriguez</strong></h4> -->
 						</div>
 						<div class="col-md-6 official ">
 							<div class="official-photo-2 mb-3">
 								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/mark.go.jpg" width="250" height="250" alt="Profile Photo">
 							</div>
 							<h3 class="h3">Hon. Mark O. Go</h3>
-							<h4 class="h4">Chairman, Committee on Higher and Technical Education of the House of Representatives</h4>
+							<h4 class="h4">Chairperson, House of Representatives Committee on Higher and Technical Education</h4>
 							<h4 class="h5">Member</h4>
 							<h4 class="h4">Represented by: <strong>Hon. Congw. Ma. Lucille L. Nava</strong></h4>
 						</div>
@@ -118,14 +118,14 @@ get_header(); ?>
 								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/erly.martir.jpg" width="250" height="250" alt="Profile Photo">
 							</div>
 							<h3 class="h3">Hon. Erly M. Martir</h3>
-							<h4 class="h4">President, Federated Faculty Association</h4>
+							<h4 class="h4">President, Federated Faculty Association, Inc.</h4>
 							<h4 class="h5">Member</h4>
 						</div>
 						<div class="col-md-4 official ">
 							<div class="official-photo-2 mb-3">
-								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/axyl.gadian.jpg" width="250" height="250" alt="Profile Photo">
+								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/vevelyn.velez.jpg" width="250" height="250" alt="Profile Photo">
 							</div>
-							<h3 class="h3">Hon. Axyl Rose T. Gadian</h3>
+							<h3 class="h3">Hon. Vevelyn H. Velez</h3>
 							<h4 class="h4">President, Federated Student Republic</h4>
 							<h4 class="h5">Member</h4>
 						</div>
@@ -134,7 +134,7 @@ get_header(); ?>
 								<img src="<?= get_stylesheet_directory_uri(); ?>/images/officials/janet.aleman.jpg" width="250" height="250" alt="Profile Photo">
 							</div>
 							<h3 class="h3">Mrs. Janet R. Aleman</h3>
-							<h4 class="h4">University Board Secretary</h4>
+							<h4 class="h4">University/Board Secretary</h4>
 						</div>
 					</div>
 				</div>
