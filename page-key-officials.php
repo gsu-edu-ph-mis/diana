58e0b661ead163d94b04c2bdd207b64276890fee
kosinix <kosinix@users.noreply.github.com>
2025-07-28 11:26:42 +0800

    Update key officials

--- page-key-officials.php
+++ page-key-officials.php
@@ -124,14 +124,14 @@ get_header(); ?>
 						</div>
 						<div class="col-md-4 official ">
 							<div class="official-photo-2 mb-3">
-								<img @click="toggle($event)" data-no="" src="<?= get_stylesheet_directory_uri(); ?>/images/officials/zzz.jpg" data-me="<?= get_stylesheet_directory_uri(); ?>/images/officials/arthur.soquena.jpg" width="250" height="250" alt="Profile">
+								<img @click="toggle($event)" data-no="" src="<?= get_stylesheet_directory_uri(); ?>/images/officials/zzz.jpg" data-me="<?= get_stylesheet_directory_uri(); ?>/images/officials/revenlie.galapin.jpg" width="250" height="250" alt="Profile">
 							</div>
-							<h3 class="h3">Dr. Arthur C. Soqueña</h3>
+							<h3 class="h3">Dr. Revenlie G. Galapin</h3>
 							<h4 class="h4">Dean, College of Business and Management (CBM)</h4>
 							<div class="h5">
 								<a href="#">
 									<svg style="width:16px;height:16px" viewBox="0 0 24 24"><path fill="currentColor" d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z" /></svg>
-									<span @click.prevent=toggleM($event) data-show="neguhe.fbdhran@tfh.rqh.cu">***click to show email***</span>
+									<span @click.prevent=toggleM($event) data-show="erirayvr.tnyncva@tfh.rqh.cu">***click to show email***</span>
 								</a>	
 							</div>
 						</div>
