d4971fe3623f2f2689f56dde0b7b34772dc8a84c
kosinix <kosinix@users.noreply.github.com>
2025-09-11 09:30:54 +0800

    Add gad

--- front-page.php
+++ front-page.php
@@ -157,6 +157,11 @@ get_header(); ?>
 						<a href="<?php echo home_url(); ?>/sustainable-development-goals/"><img src="<?= get_stylesheet_directory_uri(); ?>/images/sdg/E_SDG_logo_UN_emblem_square_trans_WEB.png" alt=""></a>
 					</div>
 				</div>
+				<div class="row pt-5">
+					<div class="col-md-12 pt-5">
+						<a href="<?php echo home_url(); ?>/gad-corner/"><img src="<?= get_stylesheet_directory_uri(); ?>/images/GAD-corner.jpg" alt="GAD"></a>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
