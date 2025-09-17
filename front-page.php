d7ef8ddb66d8a7c09932cb9b65fd4ee0fa615cca
kosinix <kosinix@users.noreply.github.com>
2025-09-17 19:00:12 +0800

    Fix mobile

--- front-page.php
+++ front-page.php
@@ -153,12 +153,12 @@ get_header(); ?>
 					</div>
 				</div>
 				<div class="row pt-5">
-					<div class="col-md-12 pt-5">
+					<div class="col-md-12 pt-5 text-center">
 						<a href="<?php echo home_url(); ?>/sustainable-development-goals/"><img src="<?= get_stylesheet_directory_uri(); ?>/images/sdg/E_SDG_logo_UN_emblem_square_trans_WEB.png" alt=""></a>
 					</div>
 				</div>
 				<div class="row pt-5">
-					<div class="col-md-12 pt-5">
+					<div class="col-md-12 pt-5 text-center">
 						<a href="<?php echo home_url(); ?>/gad-corner/"><img src="<?= get_stylesheet_directory_uri(); ?>/images/GAD-corner.png" alt="GAD"></a>
 					</div>
 				</div>
